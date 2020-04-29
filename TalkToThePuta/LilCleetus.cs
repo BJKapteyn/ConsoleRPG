b1874af31d78d14e7541ae4f51aab9fe7a8f4be5
Brad <bjkapteyn@gmail.com>
2020-04-29 17:55:56 -0400

    first commit started character creation

---
+++ TalkToThePuta/LilCleetus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TalkToThePuta
+{
+    class LilCleetus
+    {
+        public string Name = "Lil Cleetus";
+
+        public int Mass;
+        public int Intelligence;
+        public int Attitude;
+        public int Health;
+
+        public LilCleetus(int mass, int intell, int att, int health)
+        {
+            Mass = mass;
+            Intelligence = intell;
+            Attitude = att;
+            Health = health;
+        }
+
+    }
+
+
+}
